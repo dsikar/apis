c6c35283cffb3400eacc7b1a6b9c2a1f11d6dc95
Daniel Sikar <dsikar@gmail.com>
2018-05-08 11:37:42 +0100

    Added reference URL

--- hackaday/examples.sh
+++ hackaday/examples.sh
@@ -1,5 +1,8 @@
-# 1. Retrieve key from /api-key
+# As per https://dev.hackaday.io/doc/api/get-projects
+
+# Retrieve key from /api-key - supplied by hackaday.io
 APIKEY="$(cat api-key/plain-text-key)"
+
 # Retrieve one project
 # curl -G https://api.hackaday.io/v1/projects/1340?api_key=$APIKEY 
 # Projects pagination
