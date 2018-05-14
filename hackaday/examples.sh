345daf8ba90c12ad067d05204a715d868f29f138
Daniel Sikar <dsikar@gmail.com>
2018-05-14 13:59:05 +0100

    Added search example.

--- hackaday/examples.sh
+++ hackaday/examples.sh
@@ -1,8 +1,10 @@
-# As per https://dev.hackaday.io/doc/api/get-projects
+# As per https://dev.hackaday.io/doc/api
 
 # Retrieve key from /api-key - supplied by hackaday.io
 APIKEY="$(cat api-key/plain-text-key)"
 
+# search
+#curl -G https://api.hackaday.io/v1/search?api_key=$APIKEY&search_term=esp8266&page=1&per_page=1
 # Retrieve one project
 # curl -G https://api.hackaday.io/v1/projects/1340?api_key=$APIKEY 
 # Projects pagination
