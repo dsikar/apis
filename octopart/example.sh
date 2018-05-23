0f0c9a773be1bf4b7aac5389963279158323c8d0
Daniel Sikar <dsikar@gmail.com>
2018-05-23 16:33:20 +0100

    Added example.

--- octopart/example.sh
+++ octopart/example.sh
@@ -4,3 +4,7 @@ curl -G http://octopart.com/api/v3/parts/match \
    -d queries="[{\"mpn\":\"SN74S74N\"}]" \
    -d apikey=$APIKEY \
    -d pretty_print=true
+curl -G http://octopart.com/api/v3/parts/match \
+   -d queries="[{\"mpn\":\"AT90USB1286-AU\"}]" \
+   -d apikey=$APIKEY \
+   -d pretty_print=true
