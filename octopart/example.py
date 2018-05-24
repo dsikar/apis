6c4b5c93b5aa1eb1a2c3625d0aa70f3033c2d1d5
Daniel Sikar <dsikar@gmail.com>
2018-05-24 12:12:51 +0100

    Made a start on python script.

---
+++ octopart/example.py
@@ -0,0 +1,28 @@
+import pycurl
+from io import BytesIO
+import subprocess
+
+proc = subprocess.check_output("cat api-key/supplier-compare", shell=True)
+proc = proc[:-1]
+# print(proc)
+url = "http://octopart.com/api/v3/parts/match?apikey=%proc" % (proc)
+print(url)
+buffer = BytesIO()
+c = pycurl.Curl()
+c.setopt(c.URL, 'http://pycurl.io/')
+c.setopt(c.WRITEDATA, buffer)
+c.perform()
+c.close()
+
+body = buffer.getvalue()
+# Body is a byte string.
+# We have to know the encoding in order to print it to a text file
+# such as standard output.
+# print(body.decode('iso-8859-1'))
+
+# 1. Retrieve key from /api-key
+#APIKEY="$(cat api-key/supplier-compare)"
+#curl -G http://octopart.com/api/v3/parts/match \
+#   -d queries="[{\"mpn\":\"SN74S74N\"}]" \
+#   -d apikey=$APIKEY \
+#   -d pretty_print=true
