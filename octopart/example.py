ac42def7d19f80b5f1c1f38da81cec7c855b964a
Daniel Sikar <dsikar@gmail.com>
2018-06-01 09:42:44 +0100

    Iterating through objects.

--- octopart/example.py
+++ octopart/example.py
@@ -6,7 +6,6 @@ f=open("api-key/supplier-compare", "r")
 APIKEY=f.read()
 APIKEY=APIKEY.strip()
 url = "http://octopart.com/api/v3/parts/match?apikey=%s&queries=[{\"mpn\":\"SN74S74N\"}]&pretty_print=true" % (APIKEY)
-print(url)
 buffer = BytesIO()
 c = pycurl.Curl()
 # dev env workaround - https://stackoverflow.com/questions/8332643/pycurl-and-ssl-cert
@@ -17,7 +16,8 @@ c.setopt(c.WRITEDATA, buffer)
 c.perform()
 c.close()
 body = buffer.getvalue()
-parsed_json = json.loads(body.decode('iso-8859-1'))
-print(parsed_json)
-# print(body.decode('iso-8859-1'))
-
+jsonObject = json.loads(body.decode('iso-8859-1'))
+for key in jsonObject:
+    value = jsonObject[key]
+    print("The key and value are ({}) = ({})".format(key, value))
+pass
