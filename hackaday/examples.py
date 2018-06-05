7e5495a478aa3f2ec4caf2ef223e4816ed72027f
Daniel Sikar <dsikar@gmail.com>
2018-06-05 15:07:52 +0100

    Functions.

--- hackaday/examples.py
+++ hackaday/examples.py
@@ -5,31 +5,34 @@ import json
 def pagenumber(a):
   print('last page = ' + str(a))
 
-# http://pycurl.io/docs/latest/quickstart.html
-f=open("api-key/plain-text-key", "r")
-APIKEY=f.read()
-APIKEY=APIKEY.strip()
-url = "https://api.hackaday.io/v1/search?api_key=%s&search_term=esp8266&page=1&per_page=1" % (APIKEY)
-buffer = BytesIO()
-c = pycurl.Curl()
-# dev env workaround - https://stackoverflow.com/questions/8332643/pycurl-and-ssl-cert
-c.setopt(pycurl.SSL_VERIFYPEER, 0)   
-c.setopt(pycurl.SSL_VERIFYHOST, 0)
-c.setopt(c.URL, url) 
-c.setopt(c.WRITEDATA, buffer)
-c.perform()
-c.close()
-body = buffer.getvalue()
-jsonObject = json.loads(body.decode('iso-8859-1'))
-for key in jsonObject:
-    value = jsonObject[key]
-    if key == 'last_page':
-        pagenumber(value) 
-    #print("The key and value are ({}) = ({})".format(key, value))
-pass
-# print(body.decode('iso-8859-1'))
+def getApiKey():
+    f=open("api-key/plain-text-key", "r")
+    APIKEY=f.read()
+    APIKEY=APIKEY.strip()
+    return APIKEY
 
+def getPageCount():
+    APIKEY=getApiKey()
+    url = "https://api.hackaday.io/v1/search?api_key=%s&search_term=esp8266&page=1&per_page=1" % (APIKEY)
+    buffer = BytesIO()
+    c = pycurl.Curl()
+    # dev env workaround - https://stackoverflow.com/questions/8332643/pycurl-and-ssl-cert
+    c.setopt(pycurl.SSL_VERIFYPEER, 0)   
+    c.setopt(pycurl.SSL_VERIFYHOST, 0)
+    c.setopt(c.URL, url) 
+    c.setopt(c.WRITEDATA, buffer)
+    c.perform()
+    c.close()
+    body = buffer.getvalue()
+    jsonObject = json.loads(body.decode('iso-8859-1'))
+    retval = 0
+    for key in jsonObject:
+        value = jsonObject[key]
+        #print('key = ' + str(key))
+        if str(key) == 'total':
+            retval = value
+    pass
+    return retval
 
-# Paging function
-
+print(getPageCount())
 
