cab072d4b85aae310110c4f3b55c551067e6baef
Daniel Sikar <dsikar@gmail.com>
2018-06-25 14:46:27 +0100

    Spliting functions by task.

--- hackaday/examples.py
+++ hackaday/examples.py
@@ -15,7 +15,27 @@ def getApiKey():
     APIKEY=APIKEY.strip()
     return APIKEY
 
+def getURLbody(url):
+    buffer = BytesIO()
+    c = pycurl.Curl()
+    # dev env workaround - https://stackoverflow.com/questions/8332643/pycurl-and-ssl-cert
+    c.setopt(pycurl.SSL_VERIFYPEER, 0)   
+    c.setopt(pycurl.SSL_VERIFYHOST, 0)
+    c.setopt(c.URL, url) 
+    c.setopt(c.WRITEDATA, buffer)
+    c.perform()
+    c.close()
+    body = buffer.getvalue()
+    return body 
+
 def getPageCount(url):
+    body = getURLbody(url) 
+    retval = getKey('total', body);
+    # print(retval)
+    return retval
+
+
+def getPageCount2(url):
     # APIKEY=getApiKey()
     # url = "https://api.hackaday.io/v1/search?api_key=%s&search_term=esp8266&page=1&per_page=1" % (APIKEY)
     buffer = BytesIO()
