09457c264ac1a9544683c359a1537808273d5a4d
Daniel Sikar <dsikar@gmail.com>
2018-07-26 08:08:44 +0000

    Added python alias to python3.5

--- hackaday/temp/init-db.sh
+++ hackaday/temp/init-db.sh
@@ -4,18 +4,15 @@ rm fireclass.db
 sleep 0.5
 # create database and tables
 echo "Creating fireclass database, pages and menus tables..."
-python3 create-db-tables.py
+python create-db-tables.py
 sleep 0.5
 # insert data
 echo "Inserting menu data..."
-python3 menus-parse.py
-sleep 0.5
-echo "Inserting pages data..."
-python3 pages-parse.py
+python menus-parse.py sleep 0.5 echo "Inserting pages data..." python3 pages-parse.py
 # display data
 sleep 0.5
 echo "Selecting inserted menu data..."
-python333 menus-show-data.py
+python menus-show-data.py
 sleep 0.5
 echo "Selecting inserted pages data..."
-python3 pages-show-data.py
+python pages-show-data.py
