5cf903accb1949af6923d20177aa4c528fe7841b
Daniel Sikar <dsikar@gmail.com>
2018-08-06 18:45:25 +0100

    Adding basic db stats.

--- hackaday/hackaday-show-data.py
+++ hackaday/hackaday-show-data.py
@@ -9,5 +9,17 @@ rows = cur.fetchall()
 for row in rows:
 	strPrint = str(row[0]) + ", " + str(row[1]) + ", " + row[2] + ", " + row[3] + ", " + str(row[4])
 	print(strPrint)
+# todo
+# number of records
+# top instructions
+# top skulls
+# top followers
+# fwd thinking
+# most critical mass build
+# top followers
+cur.execute("SELECT * FROM tblProjects ORDER BY followers DESC LIMIT 1")
+for row in rows:
+	strPrint = str(row[0])
+	print(strPrint)
 # close connection
 conn.close()
