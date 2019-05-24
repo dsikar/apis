a9bb7a93d126b82b39c61f0e692dad531f844e44
jsikard <jsikard@jci.com>
2019-05-24 08:10:23 +0100

    Added name to top 10 lists.

--- hackaday/hackaday-show-data.py
+++ hackaday/hackaday-show-data.py
@@ -1,4 +1,11 @@
 import sqlite3
+
+def encPrint(strPrint):
+    # for strings with unprintable encoding
+    # return strPrint.encode('ascii', 'ignore')
+    # integers converted to strings, no problem
+    return strPrint
+
 # check we have all data imported
 # connect
 conn=sqlite3.connect('hackaday.db')
@@ -17,44 +24,44 @@ print("Total rows in tblProjects = " + strPrint)
 # fwd thinking
 # most critical mass build
 # top followers
-cur.execute("SELECT * FROM tblProjects ORDER BY followers DESC LIMIT 1")
+# strSQL = 'CREATE TABLE \'tblProjects\' (\'id\' INTEGER, \'owner_id\' INTEGER, \'name\' TEXT, \'summary\' TEXT, \'views\' INTEGER, \'skulls\' INTEGER, \'followers\' INTEGER, \'logs\' INTEGER, \'details\' INTEGER, \'instruction\' INTEGER, \'created\' INTEGER, \'updated\' INTEGER);' # PRIMARY KEY NOT NULL);
+
+print("Top 10 by followers")
+
+cur.execute("SELECT id, name, followers FROM tblProjects ORDER BY followers DESC LIMIT 10")
 rows = cur.fetchall()
 for row in rows:
-	strPrint = str(row[2])
-	print(strPrint)
+	strPrint = str(row[1])
+	print(encPrint(strPrint))
 # skulls
-cur.execute("SELECT * FROM tblProjects ORDER BY skulls DESC LIMIT 1")
+print("skulls")
+cur.execute("SELECT id, name, skulls FROM tblProjects ORDER BY skulls DESC LIMIT 10")
 rows = cur.fetchall()
 for row in rows:
-	strPrint = str(row[2])
-	print(strPrint)
+	strPrint = str(row[1])
+	print(encPrint(strPrint))
 # comments
-cur.execute("SELECT * FROM tblProjects ORDER BY comment DESC LIMIT 1")
+#cur.execute("SELECT * FROM tblProjects ORDER BY comment DESC LIMIT 1")
+#rows = cur.fetchall()
+#for row in rows:
+#	strPrint = str(row[1])
+#	print(encPrint(strPrint))
+
+# views
+print("Projects with most views")
+cur.execute("SELECT id, name, views FROM tblProjects ORDER BY views DESC LIMIT 10")
 rows = cur.fetchall()
 for row in rows:
-	strPrint = str(row[2])
-	print(strPrint)
+	strPrint = str(row[1])
+	print(encPrint(strPrint))
 
-# views 
-cur.execute("SELECT * FROM tblProjects ORDER BY views DESC LIMIT 1")
-rows = cur.fetchall()
-for row in rows:
-	strPrint = str(row[2])
-	print(strPrint)
-
-# instruction 
-cur.execute("SELECT * FROM tblProjects ORDER BY instruction DESC LIMIT 1")
-rows = cur.fetchall()
-for row in rows:
-	strPrint = str(row[2])
-	print(strPrint)
-
+print("Most recently created")
 # most recently created
-cur.execute("SELECT * FROM tblProjects ORDER BY created DESC LIMIT 1")
+cur.execute("SELECT id, name, created FROM tblProjects ORDER BY created DESC LIMIT 10")
 rows = cur.fetchall()
 for row in rows:
-	strPrint = str(row[2])
-	print(strPrint)
+	strPrint = str(row[1])
+	print(encPrint(strPrint))
 	
 # close connection
 conn.close()
